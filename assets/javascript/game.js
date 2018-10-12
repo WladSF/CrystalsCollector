683202a3ae9a6ddde126a1c05a17f093ba49f53d
WladSF <wpaivasf@gmail.com>
2018-10-11 18:28:50 -0700

    Made some js modifications

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 var userScore = 0;
 
 //Variable that will store the computer's choice of a random number//
-var randomNum = [];
+var randomNum = 0;
 
 //Variable for when the user wins//
 var wins = 0;
@@ -18,40 +18,60 @@ var crystal3 = Math.floor(Math.random () * 12 + 1);
 var crystal4 = Math.floor(Math.random () * 12 + 1);
 
 //Once HTML has loaded, run jQuery / JS
+console.log(Math.floor((Math.random () * 111 ) + 10));
+console.log(Math.floor((Math.random () * 111 ) + 10));
+console.log(Math.floor((Math.random () * 111 ) + 10));
+console.log(Math.floor((Math.random () * 111 ) + 10));
+console.log(Math.floor((Math.random () * 111 ) + 10));
 $(document).ready (function () {
-
-    var randomNum = Math.floor((Math.random () * 120) + 19);
-}
-var userScore = 0;
-
-
-$("#pic1").on("click", function() {
- userScore += crystal1;
-}
-
-
-$("#pic2").on("click", function() {
-    userScore += crystal2;
-   }
-
-
-$("#pic3").on("click", function() {
-    userScore += crystal3;
-   }
-
-
-$("#pic4").on("click", function() {
-    userScore += crystal4;
-   }
+    
+     var randomNum = Math.floor((Math.random () * 120) + 19);
+     $("#pic1").on("click", function() {
+        userScore += crystal1;
+       console.log("userScore", userScore);
+       });  
+       
+       $("#pic2").on("click", function() {
+           userScore += crystal2;
+           console.log("userScore", userScore);
+       });
+          
+       $("#pic3").on("click", function() {
+           userScore += crystal3;
+           console.log("userScore", userScore);
+       });
+        
+       $("#pic4").on("click", function() {
+           userScore += crystal4;
+           console.log("userScore", userScore);
+       });
+});
 
 
 
-if (userScore === randomNum {
+
+
+
+if (userScore === randomNum) {
     alert ("You won!");
     wins++ ;
-})
+}
 
 else if (userScore > randomNum) {
      alert ("You lost!");
     losses++;
+}
+
+function compareScore(){
+    if(userScore === randomNum) {
+        wins++;
+        alert("You won");
+        // Update ui
+    } else if(userScore > randomNum){
+        losses++;
+        alert("You lost");
+        // update Ui
+    } else {
+        // update ui
+    }
 }
