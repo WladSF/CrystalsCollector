2ccc20ec6b0a9ea302be8634e2360f66cca5dd13
WladSF <wpaivasf@gmail.com>
2018-10-13 16:27:59 -0700

    Final additions to homework4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,61 +24,64 @@ function reset () {
     crystal4 = Math.floor(Math.random () * 12 + 1);
     randomNum = Math.floor(Math.random () * (120 - 19 + 1)) +19;
     userScore = 0;
-    $("#targetNum").text("randomNum");
-    $("#totalScore").text("userScore");
-    //console.log("randomNum", Math.floor(Math.random () * (120 - 19 + 1)) +19);
+    $("#targetNum").text("Random Number :" + " " + randomNum);
+    $("#totalScore").text("Score :" + " " + userScore);
+    console.log("randomNum", Math.floor(Math.random () * (120 - 19 + 1)) +19);
 }
 
 reset ();
+
+function compareScore(){
+    console.log(userScore);
+    console.log(randomNum);
+        if (userScore === randomNum) {
+            wins++;
+            alert("You won!");
+            $("#totalWins").text("Wins :" + " " + wins);  // Update ui
+            reset ();
+        } 
+        
+        else if (userScore > randomNum){
+            losses++;
+            alert("You lost!");
+            $("#totalLosses").text("Losses :" + " " + losses);  // Update ui
+            reset ();
     
-     var randomNum = Math.floor(Math.random () * 120 - 19 + 1) +19;
+        }
+    
+    }
     
        $("#pic1").on("click", function() {
         userScore += crystal1;
-        //console.log("userScore", userScore);
+        $("#totalScore").text("Score :" + " " + userScore);
+        compareScore ();
+        console.log("userScore", userScore);
        });  
        
        $("#pic2").on("click", function() {
            userScore += crystal2;
-        //    console.log("userScore", userScore);
+           $("#totalScore").text("Score :" + " " + userScore);
+           compareScore ();
+           console.log("userScore", userScore);
        });
           
        $("#pic3").on("click", function() {
            userScore += crystal3;
+           $("#totalScore").text("Score :" + " " + userScore);
+           compareScore ();
         //    console.log("userScore", userScore);
        });
         
        $("#pic4").on("click", function() {
            userScore += crystal4;
+           $("#totalScore").text("Score :" + " " + userScore);
+           compareScore ();
         //    console.log("userScore", userScore);
        });
+
 });
 
-function compareScore(){
 
-    if (userScore === randomNum) {
-        wins++;
-        alert("You won!");
-        $("#totalWins").text("Wins:" + wins)  // Update ui
-        reset ();
-
-    } 
-    
-    else if (userScore > randomNum){
-        losses++;
-        alert("You lost!");
-        $("#totalLosses").text(losses)  // Update ui
-        reset ();
-
-    } 
-    
-    else {
-                                       // Update ui
-    }
-
-}
-
-compareScore ();
 
 
 
